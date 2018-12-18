61fad9b1e4ba4c15eebf717a1211120408fc0742
HsyCute <43104346+HsyCute@users.noreply.github.com>
2018-12-18 13:14:04 +0800

    Add files via upload

---
+++ 实验五/fifth/fifth/SecondViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SecondViewController.swift
+//  fifth
+//
+//  Created by student on 2018/12/15.
+//  Copyright © 2018年 . All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
